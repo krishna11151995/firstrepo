57100cdf950423f5145ee164b0252a1d7645f6b1
krishna11151995 <124556863+krishna11151995@users.noreply.github.com>
2023-02-06 12:31:58 +0530

    Add files via upload

---
+++ Pyramid.java
@@ -0,0 +1,19 @@
+package selenium_pac;
+
+public class Pyramid {
+
+	public static void main(String[] args) {
+		int i,j;
+		int n=5;
+		for(i=0;i<n;i++) {
+			
+			for(j=0; j<=i;j++) {
+				
+				System.out.println("*  ");
+			}
+			System.out.print("* ");
+		}
+
+	}
+
+}
